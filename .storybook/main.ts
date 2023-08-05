7e2ded366f55639d11f4e941c856efa0b7c77472
Mkhalled <khalled.men@gmail.com>
2023-08-05 10:26:41 +0200

    build: ajout de la conf pour storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,38 @@
+// import type { StorybookConfig } from '@storybook/nextjs';
+
+// const config: StorybookConfig = {
+//   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+//   addons: [
+//     '@storybook/addon-links',
+//     '@storybook/addon-essentials',
+//     '@storybook/addon-onboarding',
+//     '@storybook/addon-interactions',
+//   ],
+//   framework: {
+//     name: '@storybook/nextjs',
+//     options: {},
+//   },
+//   docs: {
+//     autodocs: 'tag',
+//   },
+// };
+// export default config;
+
+import type { StorybookConfig } from '@storybook/nextjs';
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
