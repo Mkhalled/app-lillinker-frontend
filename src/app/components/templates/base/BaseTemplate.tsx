5746bf9f4a8cfeea750e6e77ac5fc70b3ffb07c6
Mkhalled <khalled.men@gmail.com>
2023-08-05 11:01:57 +0200

    feat: base template for component

---
+++ src/app/components/templates/base/BaseTemplate.tsx
@@ -0,0 +1,8 @@
+import styles from './BaseTemplate.module.css';
+export interface IBaseTemplate {}
+
+const BaseTemplate: React.FC<IBaseTemplate> = () => {
+  return <div className={styles.container}>Hello world!</div>;
+};
+
+export default BaseTemplate;
