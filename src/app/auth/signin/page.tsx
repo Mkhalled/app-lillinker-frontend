7976951fd8857b0680f2b823b29600181a91952a
Mkhalled <khalled.men@gmail.com>
2023-08-14 12:16:49 +0200

    test: test

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -61,6 +61,10 @@ const SignIn: React.FC = () => {
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
                     />
 
+
+
+
+
                     <span className="absolute right-4 top-4">
                       <svg
                         className="fill-current"
